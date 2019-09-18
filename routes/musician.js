6fbab9dfef38358079d8e4ba6a85db828b65c861
Sakshi Singh <34733494+singhsakshi07@users.noreply.github.com>
2019-09-18 16:27:29 +0530

    update health check msg

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("Status: this is running ok!");
 });
 
 // retrieve all musicians from data store
